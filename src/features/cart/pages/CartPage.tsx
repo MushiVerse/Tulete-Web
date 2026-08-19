4fd24e10352cddc6e524a3780e93418dbd833db0
Norman Mushi <norlemushi737@gmail.com>
2026-08-19 17:25:00 +0300

    changed the Express services description subtitle

--- src/features/cart/pages/CartPage.tsx
+++ src/features/cart/pages/CartPage.tsx
@@ -805,7 +805,7 @@ export const CartPage = () => {
                                 <div className="text-left">
                                   <p className="text-sm font-bold">Express</p>
                                   <p className={`text-xs ${laundryPreferences.globalExpressSelected ? 'text-primary/80' : 'text-muted-foreground'}`}>
-                                    For faster Laundry Pickups and Delivery (4HRS MAX)
+                                    For Faster Laundry Pickups and Delivery (4HRS MAX)
                                   </p>
                                 </div>
                               </div>
